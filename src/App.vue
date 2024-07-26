143c18fae26cf75f19fdc290319646fa96fedaa5
simdaa <simdaa>
2024-07-26 10:14:07 +0900

    Refactor(scss): 파일 분리

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <script setup lang="ts">
-import '../src/styles/base.scss'
-import '../src/styles/common.scss'
-import '../src/styles/components/grid.scss'
-import '../src/styles/components/button.scss'
-import '../src/styles/components/form.scss'
-import '../src/styles/mgmt/common.scss'
+import '../src/styles/main.scss'
 </script>
 
 <template>
