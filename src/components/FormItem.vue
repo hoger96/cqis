eeee7ecf69277bd116a9757510e77b6b68d94218
simdaa <simdaa>
2024-08-01 15:41:00 +0900

    Feat(form): row 추가되는 UI

--- src/components/FormItem.vue
+++ src/components/FormItem.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 const props = defineProps({
+  formCol: {
+    type: Boolean,
+    required: false,
+  },
   label: {
     type: String,
     required: true,
@@ -16,9 +20,15 @@ const props = defineProps({
     type: Boolean,
     required: false,
   },
-  formCol: {
+  useColGroup: {
     type: Boolean,
     required: false,
+  },
+
+  list: {
+    type: Array,
+    required: false,
+    default: () => [],
   },
 })
 </script>
@@ -27,10 +37,17 @@ const props = defineProps({
   <div class="form__item" :class="{ 'form--col': formCol }">
     <div class="form__label-wrap">
       <label v-if="label" class="form__label" :class="{ 'is-required': required }">{{ label }}</label>
-      <slot v-if="useBtn" name="label-btn" />
+      <slot v-if="useBtn && formCol" name="label-btn" />
     </div>
     <div v-if="useGroup" class="form__group">
       <slot />
+    </div>
+    <div v-else-if="useColGroup && list" class="form__group--col">
+      <template v-for="(row, index) in list" :key="`row-${row.id}`">
+        <div class="form__group">
+          <slot :row="row" :index="index" />
+        </div>
+      </template>
     </div>
     <slot v-else />
   </div>
